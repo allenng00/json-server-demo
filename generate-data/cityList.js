b1148230749a200d9f052f6cd4a998782f8e3932
Hoàng Khang <nhk25022016@gmail.com>
2022-01-23 16:54:05 +0700

    add random cities

--- generate-data/cityList.js
+++ generate-data/cityList.js
@@ -1,3 +1,20 @@
-export const randomCityList = (fakerModule, number) => {
+const randomCityList = (fakerModule, number) => {
+	const cities = [];
 
+	if (number > 0) {
+		let regionNames = new Intl.DisplayNames(['vi'], { type: 'region' });
+
+		for (let index = 0; index < number; index++) {
+			const randomCode = fakerModule.address.countryCode();
+			const city = {
+				code: randomCode,
+				name: regionNames.of(randomCode)
+			}
+			cities.push(city);
+		}
+	}
+	// return cities;
+	return Array.from(new Set(cities));
 }
+
+module.exports = { randomCityList };
