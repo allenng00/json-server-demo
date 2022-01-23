ec2ee042e73be0b8366fe34a13b9eac886d23cc7
nhkhang2002 <nhk25022016@gmail.com>
2022-01-23 11:35:14 +0700

    custom output for respond

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const jsonServer = require('json-server')
+const queryString = require('query-string')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
 // Set default middlewares (logger, static, cors and no-cache)
@@ -16,8 +18,10 @@ server.get('/echo', (req, res) => {
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
 	if (req.method === 'POST') {
-		console.log('req :>> ', req.body);
-		req.body.createdAt = Date.now()
+		req.body.createdAt = Date.now();
+		req.body.updatedAt = Date.now();
+	}
+	else if (req.method === 'PUT' || req.method === 'PATCH') {
 		req.body.updatedAt = Date.now()
 	}
 	//
@@ -26,6 +30,31 @@ server.use((req, res, next) => {
 	// Continue to JSON Server router
 	next()
 })
+
+// Custom output for pagination
+router.render = (req, res) => {
+	// check Get with pagination
+	const headers = res.getHeaders();
+	const queryParams = queryString.parse(req._parsedUrl.query);
+	const totalCountHeader = headers['x-total-count'];
+	console.log(queryParams)
+	if (req.method === 'GET' && totalCountHeader) {
+
+		const result = {
+			data: res.locals.data,
+			pagination: {
+				_page: Number.parseInt(queryParams._page) || 1,
+				_limit: Number.parseInt(queryParams._limit) || 10,
+				_totalRows: Number.parseInt(totalCountHeader)
+			}
+		};
+		res.jsonp(result)
+	}
+
+	else {
+		res.jsonp(res.locals.data)
+	}
+}
 
 // Use default router
 server.use('/api', router)
